0b458fc001efc0767ec2f0bf934553d6d4e5be12
GrrantSmith121 <70243470+GrrantSmith121@users.noreply.github.com>
2020-08-31 20:01:20 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -15,11 +15,25 @@ assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
 #else:
 #    print("Hey! I said a number!")
 
+chances = 5
 number = random.randrange(1,10)
-guess = input("Guess a number from 1 to 10: ")
-guess = int(guess)
-if guess == number:
-    print("Great job! You got it!")
-else:
+
+while chances > 0:
+
+    guess = input("Guess a number from 1 to 10: ")
+    guess = int(guess)
+    if guess == number:
+        print("Great job! You got it!")
+        break
+    elif guess < number:
+        print("Your guess was too low! Aim for the stars!")
+        print("You have less than " + str(chances) + " chances left. ")
+    else:
+        print("Woah buddy! Your guess was way too high!")
+        print("You have less than " + str(chances) + " chances left. ")
+
+    chances -= 1
+
+if not chances > 0:
     print("Sorry, better luck next time.")
     print("The number was " + str(number))
